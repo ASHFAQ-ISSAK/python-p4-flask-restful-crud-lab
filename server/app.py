56991335aadfacc297cdb8d9bd927803c96abdb8
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-06-25 07:11:57 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -7,8 +6,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -16,37 +15,70 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
 
+class Plants(Resource):
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
 
     def post(self):
-
         data = request.get_json()
-
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
+            is_in_stock=data["is_in_stock"],
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
         return make_response(new_plant.to_dict(), 201)
 
-api.add_resource(Plants, '/plants')
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
-api.add_resource(PlantByID, '/plants/<int:id>')
-        
+    def put(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
 
-if __name__ == '__main__':
+        data = request.get_json()
+        plant.name = data.get("name", plant.name)
+        plant.image = data.get("image", plant.image)
+        plant.price = data.get("price", plant.price)
+        plant.is_in_stock = data.get("is_in_stock", plant.is_in_stock)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        data = request.get_json()
+        plant.is_in_stock = data.get("is_in_stock", plant.is_in_stock)
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
+
+
+api.add_resource(PlantByID, "/plants/<int:id>", "/plants/<int:id>/update")
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
